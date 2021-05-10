3140894ea5b0b1cc77c2945deee3a6b0e8d8260f
Eduart Uzeir <eduart.uzeir@euris.it>
2021-05-10 18:10:37 +0200

    problemi con i controller

---
+++ CinemaProject/CinemaProject/App_Start/ContainerConfig.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using CinemaProjectData.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CinemaProject
+{
+    public class ContainerConfig
+    {
+        internal static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<InMemoryCinemaData>().As<ICinema>().SingleInstance();
+            builder.RegisterType<InMemoryCinemaData>().As<ISalaCinematografica>().SingleInstance();
+            builder.RegisterType<InMemoryCinemaData>().As<ISpettatore>().SingleInstance();
+            builder.RegisterType<InMemoryCinemaData>().As<IBiglietto>().SingleInstance();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
---
+++ CinemaProject/CinemaProject/Controllers/CinemaController.cs
@@ -0,0 +1,28 @@
+﻿using CinemaProjectData.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CinemaProject.Controllers
+{
+    public class CinemaController : Controller
+    {
+        private readonly ICinema _cinema;
+        private readonly ISalaCinematografica _sala;
+        private readonly ISpettatore _spettatore;
+        private readonly IBiglietto _biglietto;
+
+        public CinemaController(ICinema cinema)
+        {
+            _cinema = cinema;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
